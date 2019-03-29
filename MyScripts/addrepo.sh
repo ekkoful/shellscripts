17877ff9028c12254ac5ff1ba2f5cc236928da24
1321735387@qq.com <1321735387@qq.com>
2019-03-29 13:25:41 +0800

    update

--- MyScripts/addrepo.sh
+++ MyScripts/addrepo.sh
@@ -11,25 +11,23 @@ yum install wget -y
 
 case "$1" in
 6|centos6|Centos6|CentOS6)
-# add CentOS6
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.rep
-
-mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
-mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup
-wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
+	# add CentOS6
+	mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+	wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.rep
+	mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
+	mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup
+	wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
 ;;
 7|centos7|Centos7|CentOS7)
-# add CentOS7
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-
-mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
-mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup
-wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+	# add CentOS7
+	mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+	wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+	mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
+	mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup
+	wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
 ;;
 k8s|kubernetes)
-# add kubernetes
+	# add kubernetes
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -40,8 +38,12 @@ repo_gpgcheck=1
 gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
 ;;
+docker|docker-ce)
+	# add docker
+
+;;
 *)
-echo "Usage: $0 6|centos6|Centos6|7|centos7|Centos7|k8s|kubernetes"
+	echo "Usage: $0 6|centos6|Centos6|7|centos7|Centos7|k8s|kubernetes"
 esac
 
 yum clean all
